b21fda9e9d6021a583362b99b84532917ad4507f
mynezius <viniciuskdepaula@gmail.com>
2022-04-20 12:50:58 -0300

    commit tcc

---
+++ QUARTO JOGO/script.js
@@ -0,0 +1,83 @@
+var canvas = document.getElementById('canvas').getContext("2d");
+
+var bg = new Bg(0,0,500,900, "assets/images/sky.png");
+var bg2 = new Bg(500,0,500,900, "assets/images/sky.png");
+
+var ground = new Ground(0,700, 500, 200, "assets/images/ground.png");
+var ground2 = new Ground(500,700, 500, 200, "assets/images/ground.png");
+
+var pipe1 = new Pipe(300, 400, 96, 358, "assets/images/pipe1.png");
+var pipe2 = new Pipe(300, -100, 96, 358, "assets/images/pipe2.png");
+
+var bird = new Bird(50, 400, 63, 51, "assets/images/bird0.png");
+
+var coin = new Coin(50, 50, 45, 65, "assets/images/3.png");
+
+var score = 0;
+var score_text = new Text();
+
+var coin_pick = new Audio("assets/sounds/point.ogg");
+var gameover = new Audio("assets/sounds/hit.ogg");
+
+var play = true;
+
+document.addEventListener("keydown", function(evento){
+    bird.vel -= 15;
+});
+
+function colision(){
+    if(bird.collide(coin)){
+        if(coin.set_visible){
+        coin.set_visible = false;
+        score += 1;
+        coin_pick.play(); 
+        }}
+    if (bird.collide(pipe1) || bird.collide(pipe2)){
+        gameover.play();
+        play = false;
+    }
+}
+
+function draw(){
+    bg.draw();
+    bg2.draw();
+    pipe1.draw();
+    pipe2.draw();
+    ground.draw();
+    ground2.draw();
+    bird.draw();
+    coin.draw();
+    score_text.draw_text(60, "Arial", 225, 100);
+}
+
+function update(){
+    if(play){
+    bg.move(2, -500, 0);
+    bg2.move(2, 0, 500)
+
+    ground.move(4, -500, 0);
+    ground2.move(4, 0, 500);
+
+    bird.move();
+    bird.animation(8, 4, "bird");
+    bird.limits();
+
+    pipe1.move(2, -100, 500, pipe2);
+
+    coin.move(pipe1);
+    coin.animation(8, 5, "");
+
+    score_text.text = score;
+
+    colision();
+    }
+}
+
+function main(){
+    canvas.clearRect(0,0,500,900);
+    requestAnimationFrame(main);
+    draw();
+    update();
+}
+
+main();
---
+++ QUARTO JOGO/util.js
@@ -0,0 +1,115 @@
+class Obj{
+
+    frame = 0;
+    timer = 0;
+    set_visible = true;
+
+    constructor(x,y,width,height, image){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.image = image;
+    }
+
+    draw(){
+        if(this.set_visible){
+            var img = new Image();
+            img.src = this.image;
+            canvas.drawImage(img, this.x, this.y, this.width, this.height);
+    }
+        }
+
+    animation(vel, limit, nome){
+        this.timer += 1;
+        if(this.timer >= vel){
+            this.timer = 0;
+            this.frame += 1;
+        }
+        if(this.frame >= limit){
+        this.frame = 0;
+        }
+        this.image = "assets/images/" + nome + this.frame + ".png"
+    }
+
+    collide(obj){
+        if(this.x < obj.x + obj.width &&
+            this.x + this.width > obj.x &&
+            this.y < obj.y + obj.height &&
+            this.y + this.height > obj.y)
+            {
+                return true;
+        }else{
+            return false;
+        }
+     }
+}
+
+    class Text{
+        text = " ";
+
+        draw_text(size, font, x, y){
+            canvas.font = size + "px" + " " + font;
+            canvas.fillStyle = "black";
+            canvas.fillText(this.text, x, y);
+        }
+    }
+
+class Bg extends Obj{
+    move(speed, limit, pos){
+        this.x -= speed;
+        if(this.x <= limit){
+            this.x = pos
+        }
+    }
+}
+
+class Ground extends Bg{
+
+}
+
+class Bird extends Obj{
+    vel = 2;
+    grav = 1;
+
+    move(){
+        if(this.vel <10){
+            this.vel += this.grav;
+        }
+        this.y += this.vel;
+    }
+
+    limits(){
+        if(this.y >=660){
+            this.y = 660;
+        }
+        else if(this.y <=0){
+            this.y = 0;
+        }
+
+    }
+}
+
+class Pipe extends Obj{
+    move(vel, limit, new_pos, pipe2){
+        this.x -= vel;
+        if(this.x <= limit){
+            this.x = new_pos;
+            this.y = Math.random() * (600 - 400) + 400;
+        }
+        pipe2.x = this.x;
+        pipe2.y = this.y - 600;
+
+    }
+}
+
+class Coin extends Obj{
+    move(pipe){
+        this.x = pipe.x + 25;
+        this.y = pipe.y - 150;
+
+        if(this.x <= -50){
+            this.set_visible = true;
+        }
+    }
+}
